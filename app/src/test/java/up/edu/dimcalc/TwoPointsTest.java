3edf5dcb19e488122f5e480b2e4d45c88b636241
Maximilian <pugliell21@up.edu>
2019-10-29 16:43:00 -0700

    Checkpoint #1

--- app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
+++ app/src/test/java/up/edu/dimcalc/TwoPointsTest.java
@@ -4,33 +4,41 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class TwoPointsTest {
+public class TwoPointsTest
+{
 
-    @Test
-    public void getPoint() {
-    }
+	@Test
+	public void getPoint()
+	{
+	}
 
-    @Test
-    public void setPoint() {
-    }
+	@Test
+	public void setPoint()
+	{
+	}
 
-    @Test
-    public void randomValue() {
-    }
+	@Test
+	public void randomValue()
+	{
+	}
 
-    @Test
-    public void setOrigin() {
-    }
+	@Test
+	public void setOrigin()
+	{
+	}
 
-    @Test
-    public void copy() {
-    }
+	@Test
+	public void copy()
+	{
+	}
 
-    @Test
-    public void distance() {
-    }
+	@Test
+	public void distance()
+	{
+	}
 
-    @Test
-    public void slope() {
-    }
+	@Test
+	public void slope()
+	{
+	}
 }
